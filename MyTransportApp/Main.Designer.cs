47112819d657f71cfe7029b2860e10b97ec3578c
Nalis1000 <nico@blatters.net>
2021-04-15 13:31:40 +0200

    Added Stationboard , optimized search suggestion

--- MyTransportApp/Main.Designer.cs
+++ MyTransportApp/Main.Designer.cs
@@ -30,28 +30,36 @@ namespace MyTransportApp
     private void InitializeComponent()
     {
       this.splitInOut = new System.Windows.Forms.SplitContainer();
-      this.tbxOutput = new System.Windows.Forms.TextBox();
+      this.chkStationBoard = new System.Windows.Forms.CheckBox();
+      this.cbxTo = new System.Windows.Forms.ComboBox();
+      this.lblDate = new System.Windows.Forms.Label();
+      this.dateDeparture = new System.Windows.Forms.DateTimePicker();
+      this.cbxFrom = new System.Windows.Forms.ComboBox();
       this.btnStop = new System.Windows.Forms.Button();
       this.btnSearch = new System.Windows.Forms.Button();
       this.lblTo = new System.Windows.Forms.Label();
-      this.tbxTo = new System.Windows.Forms.TextBox();
       this.lblFrom = new System.Windows.Forms.Label();
       this.splitDataGrid = new System.Windows.Forms.SplitContainer();
-      this.dataGridOutput = new System.Windows.Forms.DataGridView();
+      this.dataGridConnections = new System.Windows.Forms.DataGridView();
       this.rowFrom = new System.Windows.Forms.DataGridViewTextBoxColumn();
       this.rowTo = new System.Windows.Forms.DataGridViewTextBoxColumn();
       this.rowDepartureTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
       this.rowArrivalTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
       this.rowDuration = new System.Windows.Forms.DataGridViewTextBoxColumn();
-      this.tbxFrom = new System.Windows.Forms.TextBox();
+      this.dataGridStationBoard = new System.Windows.Forms.DataGridView();
+      this.rowDeparture = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.rowTrack = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.rowDestinationStation = new System.Windows.Forms.DataGridViewTextBoxColumn();
       ((System.ComponentModel.ISupportInitialize)(this.splitInOut)).BeginInit();
       this.splitInOut.Panel1.SuspendLayout();
       this.splitInOut.Panel2.SuspendLayout();
       this.splitInOut.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)(this.splitDataGrid)).BeginInit();
       this.splitDataGrid.Panel1.SuspendLayout();
+      this.splitDataGrid.Panel2.SuspendLayout();
       this.splitDataGrid.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.dataGridOutput)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridConnections)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridStationBoard)).BeginInit();
       this.SuspendLayout();
       // 
       // splitInOut
@@ -63,13 +71,15 @@ namespace MyTransportApp
       // 
       // splitInOut.Panel1
       // 
-      this.splitInOut.Panel1.Controls.Add(this.tbxOutput);
+      this.splitInOut.Panel1.Controls.Add(this.chkStationBoard);
+      this.splitInOut.Panel1.Controls.Add(this.cbxTo);
+      this.splitInOut.Panel1.Controls.Add(this.lblDate);
+      this.splitInOut.Panel1.Controls.Add(this.dateDeparture);
+      this.splitInOut.Panel1.Controls.Add(this.cbxFrom);
       this.splitInOut.Panel1.Controls.Add(this.btnStop);
       this.splitInOut.Panel1.Controls.Add(this.btnSearch);
       this.splitInOut.Panel1.Controls.Add(this.lblTo);
-      this.splitInOut.Panel1.Controls.Add(this.tbxTo);
       this.splitInOut.Panel1.Controls.Add(this.lblFrom);
-      this.splitInOut.Panel1.Controls.Add(this.tbxFrom);
       // 
       // splitInOut.Panel2
       // 
@@ -78,18 +88,56 @@ namespace MyTransportApp
       this.splitInOut.SplitterDistance = 165;
       this.splitInOut.TabIndex = 0;
       // 
-      // tbxOutput
+      // chkStationBoard
       // 
-      this.tbxOutput.Location = new System.Drawing.Point(316, 61);
-      this.tbxOutput.Name = "tbxOutput";
-      this.tbxOutput.Size = new System.Drawing.Size(100, 20);
-      this.tbxOutput.TabIndex = 6;
+      this.chkStationBoard.AutoSize = true;
+      this.chkStationBoard.Location = new System.Drawing.Point(539, 128);
+      this.chkStationBoard.Name = "chkStationBoard";
+      this.chkStationBoard.Size = new System.Drawing.Size(104, 17);
+      this.chkStationBoard.TabIndex = 11;
+      this.chkStationBoard.Text = "Departure Board";
+      this.chkStationBoard.UseVisualStyleBackColor = true;
+      this.chkStationBoard.CheckedChanged += new System.EventHandler(this.StationBoardActive);
+      // 
+      // cbxTo
+      // 
+      this.cbxTo.FormattingEnabled = true;
+      this.cbxTo.Location = new System.Drawing.Point(69, 80);
+      this.cbxTo.Name = "cbxTo";
+      this.cbxTo.Size = new System.Drawing.Size(139, 21);
+      this.cbxTo.TabIndex = 10;
+      this.cbxTo.TextUpdate += new System.EventHandler(this.InputDeparture_Changed);
+      // 
+      // lblDate
+      // 
+      this.lblDate.AutoSize = true;
+      this.lblDate.Location = new System.Drawing.Point(21, 135);
+      this.lblDate.Name = "lblDate";
+      this.lblDate.Size = new System.Drawing.Size(33, 13);
+      this.lblDate.TabIndex = 9;
+      this.lblDate.Text = "Date:";
+      // 
+      // dateDeparture
+      // 
+      this.dateDeparture.Location = new System.Drawing.Point(69, 128);
+      this.dateDeparture.Name = "dateDeparture";
+      this.dateDeparture.Size = new System.Drawing.Size(260, 20);
+      this.dateDeparture.TabIndex = 8;
+      // 
+      // cbxFrom
+      // 
+      this.cbxFrom.FormattingEnabled = true;
+      this.cbxFrom.Location = new System.Drawing.Point(69, 45);
+      this.cbxFrom.Name = "cbxFrom";
+      this.cbxFrom.Size = new System.Drawing.Size(139, 21);
+      this.cbxFrom.TabIndex = 7;
+      this.cbxFrom.TextUpdate += new System.EventHandler(this.InputDestination_Changed);
       // 
       // btnStop
       // 
       this.btnStop.Location = new System.Drawing.Point(539, 79);
       this.btnStop.Name = "btnStop";
-      this.btnStop.Size = new System.Drawing.Size(105, 23);
+      this.btnStop.Size = new System.Drawing.Size(142, 23);
       this.btnStop.TabIndex = 5;
       this.btnStop.Text = "Stop";
       this.btnStop.UseVisualStyleBackColor = true;
@@ -99,11 +147,11 @@ namespace MyTransportApp
       // 
       this.btnSearch.Location = new System.Drawing.Point(539, 45);
       this.btnSearch.Name = "btnSearch";
-      this.btnSearch.Size = new System.Drawing.Size(105, 23);
+      this.btnSearch.Size = new System.Drawing.Size(142, 23);
       this.btnSearch.TabIndex = 4;
-      this.btnSearch.Text = "Search route";
+      this.btnSearch.Text = "Search Connections";
       this.btnSearch.UseVisualStyleBackColor = true;
-      this.btnSearch.Click += new System.EventHandler(this.searchRoute);
+      this.btnSearch.Click += new System.EventHandler(this.search);
       // 
       // lblTo
       // 
@@ -113,13 +161,6 @@ namespace MyTransportApp
       this.lblTo.Size = new System.Drawing.Size(23, 13);
       this.lblTo.TabIndex = 3;
       this.lblTo.Text = "To:";
-      // 
-      // tbxTo
-      // 
-      this.tbxTo.Location = new System.Drawing.Point(69, 86);
-      this.tbxTo.Name = "tbxTo";
-      this.tbxTo.Size = new System.Drawing.Size(139, 20);
-      this.tbxTo.TabIndex = 2;
       // 
       // lblFrom
       // 
@@ -138,24 +179,28 @@ namespace MyTransportApp
       // 
       // splitDataGrid.Panel1
       // 
-      this.splitDataGrid.Panel1.Controls.Add(this.dataGridOutput);
+      this.splitDataGrid.Panel1.Controls.Add(this.dataGridConnections);
+      // 
+      // splitDataGrid.Panel2
+      // 
+      this.splitDataGrid.Panel2.Controls.Add(this.dataGridStationBoard);
       this.splitDataGrid.Size = new System.Drawing.Size(800, 520);
       this.splitDataGrid.SplitterDistance = 483;
       this.splitDataGrid.TabIndex = 0;
       // 
-      // dataGridOutput
+      // dataGridConnections
       // 
-      this.dataGridOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-      this.dataGridOutput.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+      this.dataGridConnections.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridConnections.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.rowFrom,
             this.rowTo,
             this.rowDepartureTime,
             this.rowArrivalTime,
             this.rowDuration});
-      this.dataGridOutput.Location = new System.Drawing.Point(3, 0);
-      this.dataGridOutput.Name = "dataGridOutput";
-      this.dataGridOutput.Size = new System.Drawing.Size(472, 517);
-      this.dataGridOutput.TabIndex = 0;
+      this.dataGridConnections.Location = new System.Drawing.Point(3, 0);
+      this.dataGridConnections.Name = "dataGridConnections";
+      this.dataGridConnections.Size = new System.Drawing.Size(472, 517);
+      this.dataGridConnections.TabIndex = 0;
       // 
       // rowFrom
       // 
@@ -187,15 +232,35 @@ namespace MyTransportApp
       this.rowDuration.HeaderText = "Duration";
       this.rowDuration.Name = "rowDuration";
       // 
-      // tbxFrom
+      // dataGridStationBoard
       // 
-      this.tbxFrom.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-      this.tbxFrom.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
-      this.tbxFrom.Location = new System.Drawing.Point(69, 40);
-      this.tbxFrom.Name = "tbxFrom";
-      this.tbxFrom.Size = new System.Drawing.Size(140, 20);
-      this.tbxFrom.TabIndex = 0;
-      this.tbxFrom.TextChanged += new System.EventHandler(this.InputFrom);
+      this.dataGridStationBoard.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridStationBoard.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.rowDeparture,
+            this.rowTrack,
+            this.rowDestinationStation});
+      this.dataGridStationBoard.Location = new System.Drawing.Point(3, 0);
+      this.dataGridStationBoard.Name = "dataGridStationBoard";
+      this.dataGridStationBoard.Size = new System.Drawing.Size(298, 517);
+      this.dataGridStationBoard.TabIndex = 0;
+      // 
+      // rowDeparture
+      // 
+      this.rowDeparture.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+      this.rowDeparture.HeaderText = "Departure";
+      this.rowDeparture.Name = "rowDeparture";
+      // 
+      // rowTrack
+      // 
+      this.rowTrack.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+      this.rowTrack.HeaderText = "Track";
+      this.rowTrack.Name = "rowTrack";
+      // 
+      // rowDestinationStation
+      // 
+      this.rowDestinationStation.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+      this.rowDestinationStation.HeaderText = "Destination";
+      this.rowDestinationStation.Name = "rowDestinationStation";
       // 
       // Main
       // 
@@ -205,16 +270,17 @@ namespace MyTransportApp
       this.Controls.Add(this.splitInOut);
       this.Name = "Main";
       this.Text = "Form1";
-      this.Load += new System.EventHandler(this.Main_Load);
       this.splitInOut.Panel1.ResumeLayout(false);
       this.splitInOut.Panel1.PerformLayout();
       this.splitInOut.Panel2.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)(this.splitInOut)).EndInit();
       this.splitInOut.ResumeLayout(false);
       this.splitDataGrid.Panel1.ResumeLayout(false);
+      this.splitDataGrid.Panel2.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)(this.splitDataGrid)).EndInit();
       this.splitDataGrid.ResumeLayout(false);
-      ((System.ComponentModel.ISupportInitialize)(this.dataGridOutput)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridConnections)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridStationBoard)).EndInit();
       this.ResumeLayout(false);
 
     }
@@ -222,10 +288,9 @@ namespace MyTransportApp
     #endregion
 
     private System.Windows.Forms.SplitContainer splitInOut;
-    private System.Windows.Forms.TextBox tbxTo;
     private System.Windows.Forms.Label lblFrom;
     private System.Windows.Forms.SplitContainer splitDataGrid;
-    private System.Windows.Forms.DataGridView dataGridOutput;
+    private System.Windows.Forms.DataGridView dataGridConnections;
     private System.Windows.Forms.DataGridViewTextBoxColumn rowFrom;
     private System.Windows.Forms.DataGridViewTextBoxColumn rowTo;
     private System.Windows.Forms.DataGridViewTextBoxColumn rowDepartureTime;
@@ -234,8 +299,15 @@ namespace MyTransportApp
     private System.Windows.Forms.Label lblTo;
     private System.Windows.Forms.Button btnStop;
     private System.Windows.Forms.Button btnSearch;
-    private System.Windows.Forms.TextBox tbxOutput;
-    private System.Windows.Forms.TextBox tbxFrom;
+    private System.Windows.Forms.ComboBox cbxFrom;
+    private System.Windows.Forms.Label lblDate;
+    private System.Windows.Forms.DateTimePicker dateDeparture;
+    private System.Windows.Forms.ComboBox cbxTo;
+    private System.Windows.Forms.DataGridView dataGridStationBoard;
+    private System.Windows.Forms.CheckBox chkStationBoard;
+    private System.Windows.Forms.DataGridViewTextBoxColumn rowDeparture;
+    private System.Windows.Forms.DataGridViewTextBoxColumn rowTrack;
+    private System.Windows.Forms.DataGridViewTextBoxColumn rowDestinationStation;
   }
 }
 
--- MyTransportApp/Main.cs
+++ MyTransportApp/Main.cs
@@ -14,66 +14,128 @@ namespace MyTransportApp
 {
   public partial class Main : Form
   {
-    
+
     public Main()
     {
       InitializeComponent();
     }
     ITransport transport = new Transport();
-    
+    List<Connection> connections = new List<Connection>();
+    List<StationBoard> stationboard = new List<StationBoard>();
+    private void GetStationBoard(string station)
+    {
+      string approvedstation = transport.GetStations(station).StationList[0].Name;
+      string stationid = transport.GetStations(station).StationList[0].Id;
+      ListStationBoard(station, stationid);
+      for (int i = 0; i < 5; i++)
+      {
+        dataGridStationBoard.Rows.Add(new[]
+        {
+            stationboard[i].Stop.Departure.ToString(),
+            stationboard[i].Category,
+            stationboard[i].To
+
+        });
+      }
+    }
     private void GetRoute(string departure, string arrival)
     {
-      List<string> connections = new List<string>();
-      for (int i = 0; i < transport.GetConnections(departure, arrival).ConnectionList.Count; i++)
+      ListConnection(departure, arrival);
+      for (int i = 0; i < 4; i++)
       {
-        dataGridOutput.Rows.Add(new[]
+        dataGridConnections.Rows.Add(new[]
         {
-          transport.GetConnections(departure,arrival).ConnectionList[i].From.Station.Name,
-          transport.GetConnections(departure,arrival).ConnectionList[i].To.Station.Name,
-          transport.GetConnections(departure,arrival).ConnectionList[i].From.Departure.ToString(),
-          transport.GetConnections(departure,arrival).ConnectionList[i].To.Arrival.ToString(),
-          transport.GetConnections(departure,arrival).ConnectionList[i].Duration
+          connections[i].From.Station.Name,
+          connections[i].To.Station.Name,
+          connections[i].From.Departure.Value.ToString(),
+          connections[i].To.Arrival.Value.ToString(),
+          connections[i].Duration
         });
       }
     }
 
     private void btnStop_Click(object sender, EventArgs e)
     {
-      DialogResult dialogresult=MessageBox.Show("Sind sie sicher, dass das Programm beendet werden soll?", "Programm beenden", MessageBoxButtons.YesNo,MessageBoxIcon.Error);
+      DialogResult dialogresult = MessageBox.Show("Sind sie sicher, dass das Programm beendet werden soll?", "Programm beenden", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
       if (dialogresult == DialogResult.Yes)
       {
-          Application.Exit();
+        Application.Exit();
       }
     }
 
-    private void searchRoute(object sender, EventArgs e)
+    private void search(object sender, EventArgs e)
     {
-      GetRoute(tbxFrom.Text,tbxTo.Text); 
-    }
-
-    private void Main_Load(object sender, EventArgs e)
-    {
-
-      
-    }
-
-    private void InputFrom(object sender, EventArgs e)
-    {
-      tbxFrom.AutoCompleteMode = AutoCompleteMode.Suggest;
-      tbxFrom.AutoCompleteSource = AutoCompleteSource.CustomSource;
-
-      if (tbxFrom.Text.Length >= 3)
+      if (chkStationBoard.Checked == true)
       {
-        AutoCompleteStringCollection arr = new AutoCompleteStringCollection();
-        for (int i = 0; i <= transport.GetStations(tbxFrom.Text).StationList.Count; i++)
-        {
-          arr.Add(transport.GetStations(tbxFrom.Text).StationList[i].Name);
-        }
-        tbxFrom.AutoCompleteCustomSource = arr;
+        GetStationBoard(cbxFrom.Text);
       }
+      else
+      {
+        GetRoute(cbxFrom.Text, cbxTo.Text);
+      }
+    }
+    private void ListStationBoard(string approvedstation, string id)
+    {
+      for (int i = 0; i < transport.GetStationBoard(approvedstation, id).Entries.Count; i++)
+      {
+        stationboard.Add(transport.GetStationBoard(approvedstation, id).Entries[i]);
+      }
+    }
+   
+    private void ListConnection(string departure, string arrival)
+    {
+      for (int i = 0; i < transport.GetConnections(departure, arrival).ConnectionList.Count; i++)
+      {
+        connections.Add(transport.GetConnections(departure, arrival).ConnectionList[i]);
+      }
+    }
+    public void InputDestination_Changed(object sender, EventArgs e)
+    {
+      
+      if (cbxFrom.Text.Length > 0)
+      {
+        List<Station> station = transport.GetStations(cbxFrom.Text).StationList;
+        foreach (var Stationname in station)
+        {
+          cbxFrom.Items.Add(Stationname.Name);
+        }
+      }
+    }
+
+    public void InputDeparture_Changed(object sender, EventArgs e)
+    {
+      try
+      {
+        if (cbxTo.Text.Length > 0)
+        {
+          List<Station> station = transport.GetStations(cbxTo.Text).StationList;
+          foreach (var Stationname in station)
+          {
+            cbxTo.Items.Add(Stationname.Name);
+          }
+        }
+      }
+      catch(NoNullAllowedException ex)
+      {
+        MessageBox.Show("Keine gültige Station gefunden"+ex);
+      }
+      
 
     }
-     
     
+    private void StationBoardActive(object sender, EventArgs e)
+    {
+      if (chkStationBoard.Checked == true)
+      {
+        cbxTo.Enabled = false;
+        btnSearch.Text = "Create Stationboard";
+      }
+      else
+      {
+        
+        cbxTo.Enabled = true;
+        btnSearch.Text = "Search Connections";
+      }
+    }
   }
 }
